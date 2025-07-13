2b94625f7b9274e5c2da45045e88eb35655c9dda
Tomáš Frixs <tomfrixs@gmail.com>
2025-07-13 23:39:27 +0200

    Added nuget to work with

--- src/MithgardWpf.App.Tests/UnitTest1.cs
+++ src/MithgardWpf.App.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@
         [Fact]
         public void Test1()
         {
-
+            // TODO https://www.milanjovanovic.tech/blog/enforcing-software-architecture-with-architecture-tests
         }
     }
 }
