b89012f3dc5780f2f478b9ef48cac8a5ef4683c8
Tomáš Frixs <tomfrixs@gmail.com>
2025-07-27 15:53:39 +0200

    Tests basics

---
+++ test/MithgardWpf.App.Tests.Architecture/DependencyTests.cs
@@ -0,0 +1,42 @@
+﻿using MithgardWpf.App.Core.ViewModels;
+using NetArchTest.Rules;
+using Xunit.Abstractions;
+
+namespace MithgardWpf.App.Tests;
+
+public class DependencyTests : IDisposable
+{
+    private readonly ITestOutputHelper _output;
+
+    // Setup goes here
+    public DependencyTests(ITestOutputHelper outputHelper)
+    {
+        _output = outputHelper;
+    }
+
+    // Teardown goes here
+    public void Dispose()
+    {
+        // TODO https://www.milanjovanovic.tech/blog/enforcing-software-architecture-with-architecture-tests
+        // Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+        // ITestOutputHelper, IAsyncLifetime
+    }
+
+    [Fact]
+    public void Common_ShouldNotDependOnCore()
+    {
+        // Arrange
+
+        // Act
+        var result = Types
+            .InAssembly(typeof(AppViewModel).Assembly)
+            .That()
+            .ResideInNamespace("MithgardWpf.App.Core")
+            .ShouldNot()
+            .HaveDependencyOn("MithgardWpf.App.Common")
+            .GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful, "These classes in Core reference Common:\n" + string.Join("\n", result.FailingTypeNames));
+    }
+}
---
+++ test/MithgardWpf.App.Tests.Architecture/XamlDependencyTests.cs
@@ -0,0 +1,28 @@
+﻿namespace MithgardWpf.App.Tests.Architecture;
+
+public class XamlDependencyTests
+{
+    [Fact]
+    public void Common_ShouldNotDependOnCore()
+    {
+        // Arrange
+        string project = "MithgardWpf.App";
+        string searchedNamespace = $"{project}.Core";
+        string forbiddenClrNamespace = $"clr-namespace:{project}.Common";
+        var projectRoot = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), $"..\\..\\..\\..\\..\\src\\{project}"));
+        var xamlFiles = Directory.GetFiles(projectRoot, "*.xaml", SearchOption.AllDirectories);
+
+        // Act
+        var offenders = xamlFiles
+            .Where(file =>
+            {
+                var content = File.ReadAllText(file);
+                return content.Contains($"x:Class=\"{searchedNamespace}") &&
+                       content.Contains(forbiddenClrNamespace);
+            })
+            .ToList();
+
+        // Assert
+        Assert.True(offenders.Count == 0, "These XAML files in Core reference Common:\n" + string.Join("\n", offenders));
+    }
+}
